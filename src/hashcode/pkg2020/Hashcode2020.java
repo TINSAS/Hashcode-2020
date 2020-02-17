7fef519f581e876baeb35670c3c4441dcb39ef01
Inreal <hugopons2804@gmail.com>
2020-02-17 17:20:36 +0100

    Test

--- src/hashcode/pkg2020/Hashcode2020.java
+++ src/hashcode/pkg2020/Hashcode2020.java
@@ -10,12 +10,11 @@ package hashcode.pkg2020;
  * @author Leon
  */
 public class Hashcode2020 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       System.out.println("Hello World");
     }
     
 }
