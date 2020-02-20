975fc1b037c46d0276735ff529dd446863001bca
Taydo <leon.marteau.lm@gmail.com>
2020-02-20 19:11:18 +0100

    Create Livre.java

---
+++ src/hashcode/pkg2020/Livre.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcode.pkg2020;
+
+/**
+ *
+ * @author Leon
+ */
+public class Livre {
+    int id;
+    int valeur;
+
+    public Livre(int id, int valeur) {
+        this.id = id;
+        this.valeur = valeur;
+    }
+}
