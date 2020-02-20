6df934989c359d2bbf704f934a2b5e18ce631fb1
Taydo <leon.marteau.lm@gmail.com>
2020-02-20 20:15:03 +0100

    Update Score.java

--- src/hashcode/pkg2020/Score.java
+++ src/hashcode/pkg2020/Score.java
@@ -19,12 +19,23 @@ public class Score {
         int signup = 0 ; //Compter le nombre de jour d'enregistrements
         ArrayList<Integer> books = new ArrayList(); //ArrayList des livres déjà traités
         
+        ArrayList<Livre> livresScanne;
+        /*int jour=0;
+        int jourOuvertureRestant=0;
+        while(jour<day){
+            if(jourOuvertureRestant==0){
+                
+            }
+        }*/
+        
+        
         for (int i = 0; i < Librairies.size(); i++) { //On parcourt chaque librairie
             signup = signup  + Librairies.get(i).T ;
-            for (int j = 0; j < Librairies.get(i).livresScanne.size(); j++) { //On parcourt les livres scannés par chaque librairie
-                if ((books.contains(Librairies.get(i).livresScanne.get(j).id)) == false) { //Si l'ID n'a pas été traité, on l'ajoute au score
-                    score = score + Librairies.get(i).livresScanne.get(j).valeur; //incrémentation du score
-                    books.add(Librairies.get(i).livresScanne.get(j).id); //On ajoute l'ID du livre à la liste des traités
+            livresScanne = Librairies.get(i).livresScanne;
+            for (int j = 0; j < livresScanne.size(); j++) { //On parcourt les livres scannés par chaque librairie
+                if ((books.contains(livresScanne.get(j).id)) == false) { //Si l'ID n'a pas été traité, on l'ajoute au score
+                    score = score + livresScanne.get(j).valeur; //incrémentation du score
+                    books.add(livresScanne.get(j).id); //On ajoute l'ID du livre à la liste des traités
                 }
                 if(signup == day-1){
                     score_d = score ;
@@ -32,7 +43,7 @@ public class Score {
             }
         }
         if(score_d != score){
-           throw new Error("SCORE FINAL DIFFERENT DU SCORE REEL T NUL LEON");
+            System.err.println("SCORE FINAL DIFFERENT DU SCORE REEL T NUL LEON");
         }
         return (score);
     }
