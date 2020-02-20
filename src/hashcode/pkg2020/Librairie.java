73750c645270f8347d09a0804b6923c4596b6aac
Taydo <leon.marteau.lm@gmail.com>
2020-02-20 19:07:31 +0100

    Create Librairie.java

---
+++ src/hashcode/pkg2020/Librairie.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hashcode.pkg2020;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Leon
+ */
+public class Librairie {
+    int N;
+    int T;
+    int M;
+    ArrayList<Integer> livres;
+}
