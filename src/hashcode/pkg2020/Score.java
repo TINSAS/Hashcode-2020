00a30e46fa93b86831d6251302f7cbf8d2199acd
Inreal <hugopons2804@gmail.com>
2020-02-20 19:33:39 +0100

    Fonction Score

--- src/hashcode/pkg2020/Score.java
+++ src/hashcode/pkg2020/Score.java
@@ -12,7 +12,20 @@ import java.util.ArrayList;
  * @author Benoît
  */
 public class Score {
-    public static int score(ArrayList<Integer> Pizza){
-        return 0;
+
+    public static int score(ArrayList<Librairie> Librairies) {
+        int score = 0; // Score final
+        ArrayList<Integer> books = new ArrayList(); //ArrayList des livres déjà traités
+        
+        for (int i = 0; i < Librairies.size(); i++) { //On parcourt chaque librairie
+            for (int j = 0; j < Librairies.get(i).livresScanne.size(); j++) { //On parcourt les livres scannés par chaque librairie
+                if ((books.contains(Librairies.get(i).livresScanne.get(j).id)) == false) { //Si l'ID n'a pas été traité, on l'ajoute au score
+                    score = score + Librairies.get(i).livresScanne.get(j).valeur; //incrémentation du score
+                    books.add(Librairies.get(i).livresScanne.get(j).id); //On ajoute l'ID du livre à la liste des traités
+                }
+            }
+
+        }
+        return (score);
     }
 }
